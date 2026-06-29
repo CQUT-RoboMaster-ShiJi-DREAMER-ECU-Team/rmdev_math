a051f495c67a4885893fe8237685b8c18d0b9aec
DuYicheng <duyicheng741@outlook.com>
2026-06-29 20:09:41 +0800

    增加常用矩阵，行、列向量的类型别名

--- inc/rmdev/matrix.hpp
+++ inc/rmdev/matrix.hpp
@@ -23,4 +23,28 @@ using Matrix = rmdev::ArmMatrix<Type, row, col>;
 
 #endif  // RMDEV_MATRIX_USE_CMSIS_DSP
 
+// todo 待增加 MutableMatrix 与 DynamicMatrix
+
+namespace rmdev {
+
+using Matrix2f = Matrix<float, 2, 2>;
+using Matrix3f = Matrix<float, 3, 3>;
+using Matrix4f = Matrix<float, 4, 4>;
+
+template<typename T, std::size_t dimension>
+using ColVector = Matrix<T, dimension, 1>;
+
+template<typename T, std::size_t dimension>
+using RowVector = Matrix<T, 1, dimension>;
+
+using ColVector2f = ColVector<float, 2>;
+using ColVector3f = ColVector<float, 3>;
+using ColVector4f = ColVector<float, 4>;
+
+using RowVector2f = RowVector<float, 2>;
+using RowVector3f = RowVector<float, 3>;
+using RowVector4f = RowVector<float, 4>;
+
+}  // namespace rmdev
+
 #endif  // !RMDEV_MATRIX_HPP
